7c6194a01f571e3b6292d16b50b9b313087d9e7b
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-12-14 22:04:31 +0000

    delete unecessary files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createRoot } from 'react-dom/client';
 import store from './redux/store';
 import App from './components/App';
 
