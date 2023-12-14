e62f4107762b7c3f25007c933034aaa255e1b1ad
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-12-14 22:15:35 +0000

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
+
 import store from './redux/store';
 import App from './components/App';
 
-const root = createRoot(document.getElementById('root'));root.render(
-  <Provider store={store}>
+import './index.css';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>,
+    </Provider>
+  </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
